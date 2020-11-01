709f2d9187de8077c936b4c716e250e8d5c4deda
Loyyo <maciejwiencis262@gmail.com>
2020-11-01 19:37:44 +0100

    general error handling
    - redirect if needed

--- currencies/views.py
+++ currencies/views.py
@@ -12,18 +12,24 @@ class HomeView(TemplateView):
     template_name = 'base.html'
 
     def get(self, request):
-        form = CurrencyForm()
-        value = 0
-        context = {'form': form, 'value': value}
-        return render(request, self.template_name, context)
+        try:
+            form = CurrencyForm()
+            value = 0
+            context = {'form': form, 'value': value}
+            return render(request, self.template_name, context)
+        except:
+            return redirect('/')
 
     def post(self, request):
-        form = CurrencyForm(request.POST)
-        if form.is_valid():
-            a = form.cleaned_data['currency_one'].upper()
-            amount = form.cleaned_data['amount']
-            b = form.cleaned_data['currency_two'].upper()
-            value = convert(a, b, amount)
-        context = {'form': form, 'value': value,
-                   'a': a, 'b': b, 'amount': amount}
-        return render(request, self.template_name, context)
+        try:
+            form = CurrencyForm(request.POST)
+            if form.is_valid():
+                a = form.cleaned_data['currency_one'].upper()
+                amount = form.cleaned_data['amount']
+                b = form.cleaned_data['currency_two'].upper()
+                value = convert(a, b, amount)
+            context = {'form': form, 'value': value,
+                       'a': a, 'b': b, 'amount': amount}
+            return render(request, self.template_name, context)
+        except:
+            return redirect('/')
